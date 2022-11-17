27423b41160d115c65e323e825fb7b2b4f0a6f1d
vladislav77777 <88504619+vladislav77777@users.noreply.github.com>
2022-11-17 22:35:23 +0300

    Update Others.java

--- Others.java
+++ Others.java
@@ -1,3 +1,6 @@
+/*
+This code implements the simplest model for choosing seats in a movie theater
+*/
 package Others;
 import java.util.Scanner;
 
@@ -7,25 +10,24 @@ public class Others {
     private static int rows;
     private static int columns;
     public static String menu = "1. Show the seats\n" +
-            "2. Buy a ticket\n" +
-            "3. Statistics\n" +
-            "0. Exit";
+                                "2. Buy a ticket\n" +
+                                "3. Statistics\n" +
+                                "0. Exit";
     private static int purchasedTickets = 0; // statistics
     private static float percentageTickets = 0f;
     private static int currentIncome = 0;
     private static int totalIncome = 0;
     private static boolean bought = false;
 
-
     public static void main(String[] args) {
         input();
-        int[][] seats = new int[rows][columns];
+        int[][] seats = new int[rows][columns]; // our cinema hall
 
         do {
             System.out.println(menu);
             chosenNumber = scanner.nextInt();
 
-            switch (chosenNumber) {
+            switch (chosenNumber) {           // we print our menu and the consumer chooses the option he needs 
                 case 0:
                     return;
                 case 1:
@@ -43,7 +45,7 @@ public class Others {
         } while (chosenNumber != 0);
     }
 
-    public static void input() {
+    public static void input() { // first part
         System.out.println("Enter the number of rows:");
         rows = scanner.nextInt();
         System.out.println("Enter the number of seats in each row:");
@@ -54,25 +56,25 @@ public class Others {
     public static void showTheSeats(int[][] seats) {
         System.out.println("Cinema:");
         System.out.print(" ");
-        for (int i = 0; i < columns; i++) {
+        for (int i = 0; i < columns; i++) {          // first row 1 2 3 4 ...
             System.out.print(" " + (i + 1));
         }
         System.out.println();
-        for (int i = 0; i < rows; i++) {
+        for (int i = 0; i < rows; i++) {            // left column 1 2 3 4 ...
             System.out.print(i + 1);
             for (int j = 0; j < columns; j++) {
-                if (seats[i][j] == 0)
+                if (seats[i][j] == 0)               // S - free
                     System.out.print(" S");
                 else
-                    System.out.print(" B");
+                    System.out.print(" B");         // B - seat is taken
             }
             System.out.println();
         }
         System.out.println();
     }
 
-    public static void showStatistics() {
-        float percentage = (float) purchasedTickets / (rows * columns) * 100;
+    public static void showStatistics() {                                      // useful statistics for the cinema owner
+        float percentage = (float) purchasedTickets / (rows * columns) * 100;        
         System.out.printf("Number of purchased tickets: %d\nPercentage: %.2f%%\n", purchasedTickets, percentage);
         System.out.printf("Current income: $%d\nTotal income: $%d\n", currentIncome, totalIncome);
         System.out.println();
@@ -105,19 +107,19 @@ public class Others {
             } else {
                 if (rows * columns <= 60) {
                     System.out.println("Ticket price: $" + 10);
-                    currentIncome += 10;
+                    currentIncome += 10; // count our income
                 } else {
                     if (chosen_Seat_Row <= rows / 2) {
                         System.out.println("Ticket price: $" + 10);
-                        currentIncome += 10;
+                        currentIncome += 10;                               // count our income
                     } else {
                         System.out.println("Ticket price: $" + 8);
-                        currentIncome += 8;
+                        currentIncome += 8; // count our income
                     }
                 }
                 System.out.println();
-                seats[chosen_Seat_Row - 1][chosen_Seat_column - 1] = 1;
-                purchasedTickets += 1;
+                seats[chosen_Seat_Row - 1][chosen_Seat_column - 1] = 1;    // now seat is taken
+                purchasedTickets += 1;                                     // we bought a ticket
                 bought = true;
             }
         } while (!bought);
